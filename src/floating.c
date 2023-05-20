c9908530f3add3bd88bf14af05eede129ebdf948
Matt Tyler <tylma141@student.otago.ac.nz>
2023-05-20 19:34:11 +1200

    Added datastructure for float

--- src/floating.c
+++ src/floating.c
@@ -6,10 +6,58 @@
 #include <ctype.h>
 #include <string.h>
 #include <fcntl.h>
+#include <math.h>
 
-void parse_file(char *buffer, FILE *file)
+#define SHORT_WIDTH 24
+#define LONG_WIDTH 56
+
+/*
+ * |------------------------ IBM_FLOAT Union Representation ------------------------|
+ *
+ *
+ *  For the purposes of conversion one union is used to represent both single prescision (IBM Short)
+ *  and double precision (IBM Long) formats.
+ *
+ *  The data type can be used by setting the number field with either 32 or 64 bits, the sign and
+ *  exponent remain the same between the two formats, fraction is considered to be the short (24 bits)
+ *  form of the fraction, while long_fraction has its own field.
+ *
+ *
+ * Note: Each '-' denotes a bit.
+ *
+ *                                     number
+ *                         |------------------------------------------------------------------------|
+ *                         |                                 64 bits                                 |
+ *                         |------------------------------------------------------------------------|
+ *
+ *                                     struct
+ *                         |------------------------------------------------------------------------|
+ *                         | 1 bit | 7 bits |           24 bits            |         32 bits        |
+ *                         |-------|--------|-----------------------------|------------------------|
+ *                         | sign  |exponent|          fraction           |      Not used in struct|
+ *                         |------------------------------------------------------------------------|
+ *
+ *                               long_fraction
+ *                         |------------------------------------------------------------------------|
+ *                         | 8 bits |                        56 bits                                 |
+ *                         |--------|----------------------------------------------------------------|
+ *                         |Not used|                         long_fraction                          |
+ *                         |------------------------------------------------------------------------|
+ *
+ */
+
+typedef union
 {
-}
+    uint64_t number; // Holds 64bits. Set this value with the raw binary stream.
+    struct
+    {
+        uint32_t fraction : 24; // The fraction of a Short IBM float
+        uint8_t exponent : 7;   // The exponent and sign bit are the same for both short and long.
+        uint8_t sign : 1;
+    } __attribute__((packed));   // For the compiler to not add any padding. Should work for gcc, might need to add more?
+    uint64_t long_fraction : 56; // the fraction of a Long IBM float
+
+} IBM_FLOAT;
 
 void checkError(int rs, char *msg)
 {
@@ -29,7 +77,8 @@ int main(int argc, char const *argv[])
         exit(0);
     }
 
-    FILE *file = fopen(argv[1], "rb"); // Open a binary file for reading
+    FILE *file = fopen(argv[1], "rb");
+
     if (file == NULL)
     {
         printf("Failed to open file\n");
