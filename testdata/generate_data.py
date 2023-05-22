8b42efd8d7aadf7562c710c22837722a43f4d9e5
will <frawi873@student.otago.ac.nz>
2023-05-22 15:28:52 +1200

    added expected output

--- testdata/generate_data.py
+++ testdata/generate_data.py
@@ -3,13 +3,16 @@ import segpy.ibm_float as ibm
 
 binary = open('ibm_floats.bin', 'wb')
 output = open('output.txt', 'w')
+expected = open('expected.txt', 'w')
 
 for i in range(1, 101):
     binary.write(ibm.ieee2ibm(i))
     output.write(str(i) + "\n")
+    expected.write(str(ibm.IBMFloat.from_float(i)) + "\n")
 for i in np.arange(1, 1000, 7.6543):
     binary.write(ibm.ieee2ibm(i))
     output.write(str(i) + "\n")
+    expected.write(str(ibm.IBMFloat.from_float(i)) + "\n")
 
 binary.close()
 output.close()
