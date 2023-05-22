6b7ae8924460da13de02d46de06562b9708cc59c
will <frawi873@student.otago.ac.nz>
2023-05-22 13:44:51 +1200

    added closing of files

--- testdata/generate_data.py
+++ testdata/generate_data.py
@@ -1,12 +1,15 @@
-import struct
 import numpy as np
 import segpy.ibm_float as ibm
 
 binary = open('ibm_floats.bin', 'wb')
 output = open('output.txt', 'w')
+
 for i in range(1, 101):
     binary.write(ibm.ieee2ibm(i))
-    output.write(f'{i}\n')
+    output.write(str(i) + "\n")
 for i in np.arange(1, 1000, 7.6543):
     binary.write(ibm.ieee2ibm(i))
-    output.write(f'{i}\n')
+    output.write(str(i) + "\n")
+
+binary.close()
+output.close()
