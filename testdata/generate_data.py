262fc1003f638a8b614dcfed2b856d89975385c7
frawi873 <frawi873@student.otago.ac.nz>
2023-05-22 16:10:06 +1200

    Update 3 files
    - /testdata/expected.txt
    - /README.md
    - /testdata/generate_data.py

--- testdata/generate_data.py
+++ testdata/generate_data.py
@@ -1,15 +1,20 @@
 import numpy as np
 import segpy.ibm_float as ibm
 
+# Open files (binary = the binary input for our program, output = expected output)
 binary = open('ibm_floats.bin', 'wb')
 output = open('output.txt', 'w')
 
+# For tests in our ranges,
 for i in range(1, 101):
-    binary.write(ibm.ieee2ibm(i))
-    output.write(str(ibm.IBMFloat.from_float(i)) + "\n")
+    # Write the binary bytes to the binary file...
+    binary.write(ibm.ieee2ibm(i))   
+    # ...and the expected output to the output file.
+    output.write(str(ibm.IBMFloat.from_float(i)) + "\n") 
 for i in np.arange(1, 1000, 7.6543):
     binary.write(ibm.ieee2ibm(i))
     output.write(str(ibm.IBMFloat.from_float(i)) + "\n")
 
+# Close each file to ensure everything is saved correctly.
 binary.close()
 output.close()
